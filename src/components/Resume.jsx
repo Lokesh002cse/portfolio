2db509aea720c7fa34773675cf2f488a82b8d697
Lokesh002cse <lokeshkmr995@gmail.com>
2024-08-23 01:12:22 +0530

    background

---
+++ src/components/Resume.jsx
@@ -0,0 +1,20 @@
+// src/pages/Resume.jsx
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h2>My Resume</h2>
+      <iframe
+        src="https://drive.google.com/file/d/1Hnblyk3PMLCs0ajn6tqhabie0Hp0eerT/view"  
+        width="100%"
+        height="1000px"
+        frameBorder="0"
+        allow="autoplay"
+        title="Resume"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Resume;
